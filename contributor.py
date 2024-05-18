706a03511c5f843fcf315139e27bf6ec5c3bb6d8
Minji Son <109676875+midday2612@users.noreply.github.com>
2024-05-18 19:44:39 +0900

    [FIX] contributor 동작 방식 수정

--- contributor.py
+++ contributor.py
@@ -13,13 +13,7 @@ def contributor_ver(webdriver_path, github_url, id):
 
     # 해당 URL에 요청 보내기
     driver.get(github_url + "/graphs/contributors")
-    time.sleep(2)  # 페이지가 로드될 때까지 대기
-
-    # 페이지 소스 가져오기
-    page_source = driver.page_source
-
-    # BeautifulSoup을 사용하여 HTML 파싱
-    soup = BeautifulSoup(page_source, 'html.parser')
+    time.sleep(5)  # 페이지가 로드될 때까지 대기
 
     contributor_names = driver.find_elements(By.CSS_SELECTOR, ".text-normal")
     for i in contributor_names:
@@ -36,3 +30,5 @@ def contributor_ver(webdriver_path, github_url, id):
         print("해당 지원자의 정보가 없습니다.")
 
     print(id) # 이름이 정상적으로 담기는지 확인
+
+contributor_ver(r"C:\Users\minji\Downloads\chromedriver_win32 (1)\chromedriver.exe","https://github.com/midday2612/aix-deep-learning", "midday2612")
